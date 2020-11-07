7ab0a697eef68e90d4b9f0bff0576829dc289c37
Pablo Sánchez Bello <pablo@MacBook-Air-de-Pablo.local>
2020-11-07 09:33:33 +0100

    Splash screen and login page without backend functionallity

--- app/src/main/java/com/developm8/treina/LoginActivity.kt
+++ app/src/main/java/com/developm8/treina/LoginActivity.kt
@@ -1,12 +1,30 @@
 package com.developm8.treina
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.switchmaterial.SwitchMaterial
+import com.google.android.material.textfield.TextInputLayout
+import com.google.android.material.textview.MaterialTextView
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val kindPerson : MaterialTextView = findViewById(R.id.materialTextViewKindPerson)
+        val switchKindPerson : SwitchMaterial = findViewById(R.id.switchMaterialKindPerson)
+        val textInputLayoutTrainerCode : TextInputLayout = findViewById(R.id.textInputLayoutTrainerCode)
+
+        switchKindPerson.setOnClickListener {
+            if (switchKindPerson.isChecked) {
+                kindPerson.setText(R.string.kindPersonTrainer)
+                textInputLayoutTrainerCode.visibility = View.GONE
+            } else {
+                kindPerson.setText(R.string.kindPersonClient)
+                textInputLayoutTrainerCode.visibility = View.VISIBLE
+            }
+        }
     }
 }
